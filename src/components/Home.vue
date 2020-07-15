6a6eb9afab536d04851a29124450f5ca8320ec85
awsome-cats <cattwin82@gmail.com>
2020-07-15 14:57:24 +0900

    Initial Apps & Create Header, Home, Menu

---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="background">
+    <div class="background-overlay">
+      <h2>welcome to pizza planet</h2>
+      <img src="../assets/images/roller.png" alt="">
+      <h3>feeling hungry?</h3>
+      <button class="order_btn">注文する</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+.background {
+  background: url('../assets/images/dough.jpg') center;
+  background-size: cover;
+  height:30vh;
+  display: flex;
+  text-align: center;
+  align-items: center;
+}
+.background-overlay {
+  display: flex;
+  flex-direction: column;
+  background: #f1e6da;
+  opacity: 0.8;
+  width: 50%;
+  margin: 0 auto;
+  padding: 5px;
+  align-items: center;
+}
+
+.background-overlay > img {
+  max-width: 15%;
+}
+.order_btn {
+  background: #f79f38;
+  border-radius: 3px;
+  margin-bottom: 10px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+</style>
